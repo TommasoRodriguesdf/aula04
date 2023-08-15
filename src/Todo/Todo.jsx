62e61a79e695b658a64aa2420bb09b84c343ec73
TommasoRodriguesdf <131806144+TommasoRodriguesdf@users.noreply.github.com>
2023-08-15 09:18:10 -0400

    toto
    rede

---
+++ src/Todo/Todo.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Todo(){
+    return (
+     <div>
+        <h1>todo</h1>
+        <Link to="/home"> HOME </Link>
+     </div>
+     
+    );
+
+
+}
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Todo from './Todo/Todo.jsx'
+import Home from './Home/Home.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+    <Route path= '/home' element= {<Home />}></Route>
+    <Route path= '/todo' element= {<Todo />}></Route>
+    </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
